19a2a77016dd971bea1d661e190e9efa2aa67a20
Gonza Nardini <nardinigonzalom@gmail.com>
2024-09-14 04:17:20 -0300

    title

--- pages/+config.ts
+++ pages/+config.ts
@@ -1,14 +1,13 @@
-import vikeReact from "vike-react/config";
-import type { Config } from "vike/types";
-import Head from "../layouts/HeadDefault.js";
-import Layout from "../layouts/LayoutDefault.js";
+import vikeReact from 'vike-react/config';
+import type { Config } from 'vike/types';
+import Head from '../layouts/HeadDefault.js';
+import Layout from '../layouts/LayoutDefault.js';
 
 // Default config (can be overridden by pages)
 export default {
   Layout,
   Head,
 
-  // <title>
-  title: "My Vike App",
+  title: 'Phinxer Analytice',
   extends: vikeReact,
 } satisfies Config;
