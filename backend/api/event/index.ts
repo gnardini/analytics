f5b32b1444e2752ef377ca17bd3fa71b4c9d6f07
Gonza Nardini <nardinigonzalom@gmail.com>
2024-09-18 17:46:17 -0300

    skip bots

--- backend/api/event/index.ts
+++ backend/api/event/index.ts
@@ -3,6 +3,7 @@ import { eventSchema } from '@backend/schemas/event';
 import { ClickHouseService } from '@backend/services/ClickHouseService';
 import OrganizationsService from '@backend/services/OrganizationsService';
 import { TrackingService } from '@backend/services/TrackingService';
+import { isBot } from '@backend/utils/userAgent';
 
 export default createApiHandler({
   method: 'POST',
@@ -11,6 +12,10 @@ export default createApiHandler({
     const organization = await OrganizationsService.getOrganizationByName(data.domain);
     if (!organization) {
       throw new ApiError(401, 'Invalid domain');
+    }
+
+    if (isBot(req.headers['user-agent'])) {
+      return { success: false, reason: 'isBot' };
     }
 
     const trackingData = TrackingService.track(data, req, res, organization.id);
